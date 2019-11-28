cb9316e1131ebcf2390230e6ae159867d94ff5f7
Erfan Kheyrollahi <ekm507@gmail.com>
2019-11-29 00:13:54 +0330

    it works now :D

--- de-ask.py
+++ de-ask.py
@@ -12,7 +12,7 @@ frequency = 1000.0 # Hz
 # sample duration in seconds
 duration = 0.01 # seconds
 # norm amplitude of zeros in modulated audio
-zeroamp = 0.90
+zeroamp = 0.50
 # audio file to be read from
 audiofilename = "test.wav"
 
@@ -25,40 +25,15 @@ fileSize = audiofile.getparams()[3]
 # get sample rate of recorded file
 sample_rate = audiofile.getparams()[2]
 
-# extract file into a list
-audio_samples = audiofile.readframes(fileSize)
 
+# extract file into a list
+audio = []
+for i in range(fileSize):
+    x = audiofile.readframes(1)
+    audio.append(struct.unpack('h', x))
 
 # number of samples for each symbol
 num_symbol_samples = duration * sample_rate
-
-def byte2int(byte):
-    if byte > 127:
-        return (256-byte) * (-1)
-    else:
-        return byte
-
-def twoByte2int(byteH, byteL):
-    f = (byteH << 8) | byteL
-    if(byteH > 127):
-        return (-1) * (2 ** 16 - f)
-    else:
-        return f
-
-def fourByte2int(b0, b1, b2, b3):
-    f = b0 << 24
-    f |= (b1 << 16)
-    f |= (b2 << 8)
-    f |= b3
-    if(b0 > 127):
-        return (-1) * (2 ** 32 - f)
-    else:
-        return f
-
-zipedAudio = zip(*[iter(audio_samples)] * 2)
-
-# use numpy arrays
-audio = np.array([twoByte2int(b1, b2) for b1, b2 in zipedAudio])
 
 
 """
@@ -83,4 +58,4 @@ amps /= np.max(amps)
 # quantize amps to get data
 data = 1 * (amps > zeroamp)
 
-print(amps)
+print(data)
