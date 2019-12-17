4aedcf0bb91aaad44e21b04cd5031b291051e841
Erfan Kheyrollahi <ekm507@gmail.com>
2019-12-17 16:48:16 +0330

    let's demodulate

---
+++ de-fsk.py
@@ -0,0 +1,44 @@
+# ASK demodulation
+
+# used for editing wave files
+import wave
+# for basic mathematic operations
+import numpy as np
+# for using with wave functions
+import struct
+# used for getting arguments
+import sys
+# used for calculating hash sum of data
+import hashlib
+
+# frequency of ASK
+frequency_list = [1000.0, 2000.0] # Hz
+# number of symbol types. for mFSK 2 is enough.
+num_frequency = 2
+# sample duration in seconds
+duration = 0.001 # seconds
+
+# audio file to be read from
+try:
+    audiofilename = sys.argv[1]
+except IndexError:
+    audiofilename = "test.wav"
+
+# open audio file
+audiofile = wave.open(audiofilename, 'r')
+
+# get number of frames in file
+fileSize = audiofile.getparams()[3]
+
+# get sample rate of recorded file
+sample_rate = audiofile.getparams()[2]
+
+
+# extract file into a list
+audio = []
+for i in range(fileSize):
+    x = audiofile.readframes(1)
+    audio.append(struct.unpack('h', x))
+
+# number of samples for each symbol
+num_symbol_samples = duration * sample_rate
